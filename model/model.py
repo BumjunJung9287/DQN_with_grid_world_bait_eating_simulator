c7083a845ebcc7fa82ac135a8b20f0e000c5110f
Bumjun Jung <47442084+BumjunJung9287@users.noreply.github.com>
2019-12-01 19:59:24 +0900

    Add files via upload

---
+++ model/model.py
@@ -0,0 +1,130 @@
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+class DeepQNetwork(nn.Module):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, fc3_dims, n_actions):
+        super(DeepQNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.fc3_dims = fc3_dims
+        self.n_actions = n_actions
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.fc3 = nn.Linear(self.fc2_dims, self.fc3_dims)
+        self.fc4 = nn.Linear(self.fc3_dims, self.n_actions)
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss() # MSE:mean square error
+
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu:0')
+        #cuda:0 -> to use first GPU (if it is available), cuda:1 -> second GPU
+        self.to(self.device)
+    def change_lr(self, lr):
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+
+    def forward(self, observation):
+        state = T.Tensor(observation).to(self.device)
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        actions = self.fc4(x)
+        return actions
+
+class Agent(object): # subject which do the actions
+    def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions,
+                 max_mem_size=1000000, eps_end=0.01, eps_dec=0.996):
+        self.gamma = gamma
+        self.epsilon = epsilon # threshold for randomness
+        # if rand<eps -> random choice, else:predicted choice
+        self.eps_min = eps_end
+        self.eps_dec = eps_dec
+        self.batch_size = batch_size
+        self.action_space = [i for i in range(n_actions)]
+        self.mem_size = max_mem_size
+        self.mem_cntr = 0
+        self.n_actions = n_actions
+        # mem_cntr : index of how many memories it stored
+        self.Q_eval = DeepQNetwork(lr, n_actions=self.n_actions,
+                                  input_dims=input_dims, fc1_dims=256,
+                                  fc2_dims=256, fc3_dims=100)
+        self.state_memory = np.zeros((self.mem_size, *input_dims))
+        self.new_state_memory = np.zeros((self.mem_size, *input_dims))
+        self.action_memory = np.zeros((self.mem_size, self.n_actions),
+                                     dtype=np.uint8) # becareful of data type on pytorch
+        self.reward_memory = np.zeros(self.mem_size)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.uint8)
+        # -> sequence of done flags
+
+    def change_lr(self, lr):
+        self.Q_eval.change_lr(lr)
+
+    def store_transition(self, state, action, reward, state_, terminal):
+        index = self.mem_cntr % self.mem_size
+        # index will go back to zero when mem_cntr gets to self.mem_size (over writing)
+        self.state_memory[index] = state
+        actions = np.zeros(self.n_actions)
+        actions[action] = 1.0 # one_hot encoding
+        self.action_memory[index] = actions
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = 1 - terminal
+        self.new_state_memory[index] = state_
+        self.mem_cntr += 1
+
+    def choose_action(self, observation):
+        rand = np.random.random()
+        if rand < self.epsilon:
+            action = np.random.choice(self.action_space)
+        else:
+            actions = self.Q_eval.forward(observation)
+            action = T.argmax(actions).item() # Tensor -> integer
+        return action
+    def learn(self):
+        if self.mem_cntr > self.batch_size:
+            # at first the correlation of samples are strong
+            # so start training when self.mem_cntr > self.batch_size
+            self.Q_eval.optimizer.zero_grad()
+            max_mem = self.mem_cntr if self.mem_cntr < self.mem_size \
+                        else self.mem_size
+            batch = np.random.choice(max_mem, self.batch_size)
+                # np.random.choice(a,b) : choose b numbers which are less than max_mem
+
+            state_batch = self.state_memory[batch]
+
+            action_batch = self.action_memory[batch]
+            action_values = np.array(self.action_space, dtype=np.uint8)
+            action_indices = np.dot(action_batch, action_values)
+
+            reward_batch = self.reward_memory[batch]
+            terminal_batch = self.terminal_memory[batch]
+            new_state_batch = self.new_state_memory[batch]
+
+            reward_batch = T.Tensor(reward_batch).to(self.Q_eval.device)
+            terminal_batch = T.Tensor(terminal_batch).to(self.Q_eval.device)
+
+            q_eval = self.Q_eval.forward(state_batch).to(self.Q_eval.device)
+
+            #duplication
+            # q_target = q_eval is wrong! assignment by regerence not value
+            # want q_target = q_eval for all a except max action
+            q_target = self.Q_eval.forward(state_batch).to(self.Q_eval.device)
+            #q_target = q_eval.clone()
+            # did not seperated taget network cause it seems to work..?
+
+            q_next = self.Q_eval.forward(new_state_batch).to(self.Q_eval.device)
+
+            batch_index = np.arange(self.batch_size, dtype=np.int32) # [i for i in range(batch_size)]
+            target_update = reward_batch + \
+                            self.gamma*T.max(q_next, dim=1)[0]*terminal_batch
+            for i in range(len(batch_index)):
+                q_target[batch_index[i], action_indices[i]] = target_update[i]
+            #q_target[batch_index, action_indices] = reward_batch + \
+            #                self.gamma*T.max(q_next, dim=1)[0]*terminal_batch
+
+            self.epsilon = self.epsilon*self.eps_dec if self.epsilon > \
+                        self.eps_min else self.eps_min
+            loss = self.Q_eval.loss(q_target, q_eval).to(self.Q_eval.device)
+            loss.backward() # back propagation
+            self.Q_eval.optimizer.step()
---
+++ util/utils.py
@@ -0,0 +1,171 @@
+import torch
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+from model.model import DeepQNetwork
+class game_env():
+    def __init__(self, i):
+        self.episod = i
+        self.obstical = []
+        self.init_state = random_position_except(self.obstical)
+        self.obstical.append(self.init_state)
+        self.goal = random_position_except(self.obstical)
+        self.obstical.append(self.goal)
+        self.state = self.init_state[:]
+        self.action = None
+        self.start = True
+
+
+    def reset(self, init_state):
+        self.obstical = []
+        self.init_state = init_state[:]
+        self.obstical.append(self.init_state)
+        self.goal = random_position_except(self.obstical)
+        self.obstical.append(self.goal)
+        self.state = self.init_state[:]
+        self.action = None
+        self.start = True
+
+        self.observation = []
+        observation = [self.state, self.goal]
+        for slist in observation:
+            for s in slist:
+                self.observation.append(s) # currenct state + goal_state
+        return self.observation
+
+    def step(self, action, step):
+        self.action = action
+        self.last_state = self.state[:]
+        if action == 0:
+            self.state[1] += 1
+        elif action == 1:
+            self.state[1] -= 1
+        elif action == 2:
+            self.state[0] -= 1
+        elif action == 3:
+            self.state[0] += 1
+
+        self.observation = []
+        observation = [self.state, self.goal]
+        for slist in observation:
+            for s in slist:
+                self.observation.append(s) # currenct state + goal_state
+
+        reward = 0
+        last_distance = np.linalg.norm(np.array(self.last_state) - np.array(self.goal))
+        current_distance = np.linalg.norm(np.array(self.state) - np.array(self.goal))
+        reward = (last_distance - current_distance)
+        print("distances",last_distance, current_distance)
+        print("distance_reward", reward)
+        reward -= step
+        print("step:",step)
+        if self.state == self.goal:
+            self.reward = 10
+        else:
+            self.reward = reward
+        if check_end(self.state):
+            self.done = True
+            self.reward = -10
+        else:
+            self.done = 0
+        if step > 10:
+            self.reward = -3
+            self.done = True
+            print("too late")
+        self.start = False
+        return self.observation, self.reward, self.done
+
+    def visualize(self):
+        visualize(self.init_state, self.goal, self.state, self.action, self.episod, start=self.start)
+
+
+def random_position_except(obstical_list):
+    while True:
+        x = np.random.choice(np.arange(0,4))
+        y = np.random.choice(np.arange(0,6))
+        xy = list([x,y])
+        if xy not in (obstical_list):
+            return xy
+
+def check_end(state):
+    x = state[0]
+    y = state[1]
+    if x<0 or x>3 or y<0 or y>5:
+        return True
+    else:
+        return False
+
+def save_model(name, brain, dqn_net, optimizer):
+    print("Saving model... wait...")
+    memories = []
+    memories.append(brain.state_memory)
+    memories.append(brain.new_state_memory)
+    memories.append(brain.action_memory)
+    memories.append(brain.reward_memory)
+    memories.append(brain.terminal_memory)
+
+    torch.save(
+        {
+            "dqn": dqn_net.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            "memories": memories
+        },
+        name,
+    )
+    print("Model saved!")
+
+def load_model(md_name, n_actions, device, lr):
+    dqn_net = DeepQNetwork(lr, [4], 256, 256, 100, 4).to(device)
+    optimizer = optim.Adam(dqn_net.parameters(), lr=lr)
+    memories = None
+    try:
+        checkpoint = torch.load(md_name)
+        dqn_net.load_state_dict(checkpoint["dqn"])
+        optimizer.load_state_dict(checkpoint["optimizer"])
+        memories = checkpoint["memories"]
+        print("memories",memories)
+
+        print("Models loaded!")
+    except Exception as e:
+        print(f"Couldn't load Models! => {e}")
+    return dqn_net, optimizer, memories
+
+
+def visualize(init_state, goal, state, last_action, episod, start=False):
+    print("episod %d",episod)
+    arrows = {0:"►", 1:"◄", 2:"▲", 3:"▼"}
+    map = [["#" for i in range(6)] for j in range(4)]
+    map[goal[0]][goal[1]] = "G"
+    map[init_state[0]][init_state[1]] = "S"
+    print("init_state:", init_state)
+    if not start:
+        map[state[0]][state[1]] = arrows[last_action]
+    else:
+        print("start game!")
+    for i in range(4):
+        string = ""
+        for j in range(6):
+            string += map[i][j]
+            string += " "
+        print(string[:-1])
+        print()
+    print("############")
+    if goal == state:
+        print("ate bait!! genterating next goal!")
+    return
+
+
+if __name__=="__main__":
+    score = 0
+    visualize((0,0),(3,5),(0,0),None,0,start=True)
+    visualize((0,0),(3,5),(0,1),0,0)
+    visualize((0,0),(3,5),(0,2),0,0)
+    visualize((0,0),(3,5),(0,3),0,0)
+    visualize((0,0),(3,5),(0,4),0,0)
+    visualize((0,0),(3,5),(0,5),0,0)
+
+    visualize((0,0),(3,5),(1,5),3,0)
+    visualize((0,0),(3,5),(2,5),3,0)
+    visualize((0,0),(3,5),(3,5),3,0)
